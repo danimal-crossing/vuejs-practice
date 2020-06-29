3358d5c450bdebd138da0ed1d2215094e212dcf5
dani <dani.zaghian@gmail.com>
2020-06-29 11:10:29 -0700

    adds v-if and logic

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var app = new Vue({
   el: "#app",
   data: {
     message: "Hello from JavaScript!",
-    name: "Dani"
+    name: "Dani",
+    showExtraInformation: true
   },
   methods: {
     changeMessage: function () {

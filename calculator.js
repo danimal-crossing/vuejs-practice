8108119a206711ed269297eb974f7b893a32fcea
dani <dani.zaghian@gmail.com>
2020-06-29 10:43:14 -0700

    adds working calculator

---
+++ calculator.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello Vue!",
+    number1: 0,
+    number2: 0,
+    number3: 0,
+    result: 0
+  },
+  methods: {
+    computeSum: function () {
+      this.result = parseInt(this.number1) + parseInt(this.number2) + parseInt(this.number3);
+    }
+  }
+});
